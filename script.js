bd68bd89fac9d55edafa960d219a39b856236b7f
Zupi <49796750+Zupi-HR@users.noreply.github.com>
2026-02-20 14:30:43 +0100

    Refactor validation logic and imrove error borders

--- script.js
+++ script.js
@@ -9,25 +9,38 @@ form.addEventListener("submit", (e) => {
     return;
   }
   formElements.forEach((element) => {
-    if (element.type === "radio") {
-      element.parentElement.classList.remove("input-error");
-    } else {
-      element.classList.remove("input-error");
-    }
-    let parentElement = element.parentElement;
-    let span =
-      element.parentElement.querySelector(".error") ||
-      element.closest("fieldset")?.querySelector(".error");
-    if (span) {
-      span.style.display = "none";
-      if (!element.checkValidity()) {
-        span.style.display = "block";
-        if (element.type === "radio") {
-          element.parentElement.classList.add("input-error");
-        } else {
-          element.classList.add("input-error");
-        }
-      }
-    }
+    hideValidationError(element);
+    showValidationError(element);
   });
 });
+
+function hideValidationError(element) {
+  let span =
+    element.parentElement.querySelector(".error") ||
+    element.closest("fieldset")?.querySelector(".error");
+  if (span) {
+    span.style.display = "none";
+  }
+  if (element.type === "radio") {
+    element.parentElement.classList.remove("input-error");
+  } else {
+    element.classList.remove("input-error");
+  }
+}
+
+function showValidationError(element) {
+  let parentElement = element.parentElement;
+  let span =
+    parentElement.querySelector(".error") ||
+    element.closest("fieldset")?.querySelector(".error");
+  if (element.validity.valueMissing) {
+    span.style.display = "block";
+    if (element.type === "radio") {
+      element.parentElement.classList.add("input-error");
+    } else {
+      element.classList.add("input-error");
+    }
+  } else if (element.validity.typeMismatch) {
+    span = parentElement.querySelector("email-error");
+  }
+}
